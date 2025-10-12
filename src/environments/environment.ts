5947c34ef0f220e95dff0ad6911e1e4751d8f072
MO1993OR <serignemorba@icloud.com>
2025-10-12 20:32:40 +0000

    fixed bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 
 export const environment={
     //baseAPIURL:'https://anish-mockapi.onrender.com/'
+   // baseAPIURL:'http://localhost:4001/api/'
     baseAPIURL: 'https://keur-mamou-khaleyi-backend.onrender.com/api/'
 }
